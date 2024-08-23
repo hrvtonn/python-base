e3062b1723b3aea0aa87e03eda0b29a4acebf281
hrvtonn <heryvelton20@gmail.com>
2024-08-23 14:39:54 -0300

    new version

--- hello.py
+++ hello.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
+"""Hello World Multi Lingua.
 
+Dependendo da lingua configurada no ambiente o programa exibe a
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+     export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Hery"
+__licence__ = "Unlicense"
+
+# Este programa imprime Hello World
 print("hello world")
