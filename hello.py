036a24d7ffb6668b3ad5d5a2732d81137d52e025
hrvtonn <heryvelton20@gmail.com>
2024-08-23 21:41:24 -0300

    Implementamos o Hello World Multi Linguas

--- hello.py
+++ hello.py
@@ -20,7 +20,19 @@ __version__ = "0.0.1"
 __author__ = "Hery"
 __licence__ = "Unlicense"
 
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
 
 
-# Este programa imprime Hello World
-print("hello world")
+print(msg)
